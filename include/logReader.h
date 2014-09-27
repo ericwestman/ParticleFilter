590c35341affb34134c457db408cf28257fc4ba6
Eric Westman <ewestman@gs14410.sp.cs.cmu.edu>
2014-09-27 13:51:01 -0400

    Added bare bones of log reader

--- include/logReader.h
+++ include/logReader.h
@@ -1,3 +1,25 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+
+struct LaserData{
+	// Robot pose
+	float x, y, theta;
+	// Laser Pose (approximately 25 cm offset forward from true center of robot)
+	float xl, yl, thetal;
+	// Range readings of laser in cm. 180 readings span 180 degrees 
+	// starting right, going left
+	float r[180];
+	// Timestamp of laser reading
+	float ts;
+};
+
+struct OdometryData{
+  // Coordinates of robot in standard odometry frame
+	float x, y, theta;
+  // Timestamp of odometry reading
+  float ts;
+};
+
+void read_log(char *logName, std::vector<LaserData> *logLaser, 
+                             std::vector<OdometryData> *logOdometry);
--- src/particleFilter.cpp
+++ src/particleFilter.cpp
@@ -2,6 +2,8 @@
 #include <mapReader.h>
 #include <logReader.h>
 
+using namespace std;
+
 int main()
 {
 	// Load occupancy map of wean hall
@@ -10,6 +12,10 @@ int main()
   read_map(weanMapName, weanMap);
 
   // Load data from the log
+  char logName[] = "../data/log/robotdata1.log";
+  vector<LaserData> *myLaserData = new vector<LaserData>;
+  vector<OdometryData> *myOdometryData = new vector<OdometryData>;
+  read_log(logName, myLaserData, myOdometryData);
 
   std::cout << "Done!\n";
 }
